7a8004c121663e8a5c5b13cd9b019d5a0c943fd3
qcydm <42266769+qcydm@users.noreply.github.com>
2021-04-03 14:56:47 +0800

    AANE description
    一种网络嵌入技术，AANE的优点在于它可以把嵌入工作同时交给多个workers独立、同时间完成，大大地节省了时间。
    对比一般的网络嵌入方法，如PCA（主成分分析）、LINE（Large-scale Information Network Embedding）、DeepWalk等，AANE算法在很多数据集上表示出了提取特征的优越性，因此，它可以作为提取网络特征的有效工具。
    代码来源：（base on python）:https://github.com/xhuang31/AANE_Python
    （base on Matlab）:https://github.com/xhuang31/AANE_MATLAB

---
+++ Feature dimension reduction/AANE_fun.m
@@ -0,0 +1,130 @@
+function H = AANE_fun(Net,Attri,d,varargin)
+%Jointly embed Net and Attri into embedding representation H
+%     H = AANE_fun(Net,Attri,d);
+%     H = AANE_fun(Net,Attri,d,lambda,rho);
+%     H = AANE_fun(Net,Attri,d,lambda,rho,maxIter);
+%     H = AANE_fun(Net,Attri,d,lambda,rho,maxIter,'Att');
+%     H = AANE_fun(Net,Attri,d,lambda,rho,maxIter,'Att',splitnum);
+% 
+%         Net   is the weighted adjacency matrix
+%        Attri  is the attribute information matrix with row denotes nodes
+%         d     is the dimension of the embedding representation
+%       lambda  is the regularization parameter
+%         rho   is the penalty parameter
+%       maxIter is the maximum number of iteration
+%        'Att'  refers to conduct Initialization from the SVD of Attri
+%      splitnum is the number of pieces we split the SA for limited cache
+
+%   Copyright 2017 & 2018, Xiao Huang and Jundong Li.
+%   $Revision: 1.0.2 $  $Date: 2018/02/19 00:00:00 $
+
+%% Parameters
+maxIter = 2; % Max num of iteration
+n = size(Net,1); % Total number of nodes
+Net(1:n+1:n^2) = 0;
+lambda = 0.05; % Initial regularization parameter
+rho = 5; % Initial penalty parameter
+splitnum = 1;
+if length(varargin) >= 4 && strcmp(varargin{4},'Att')
+    MaxEdges = randperm(size(Attri,2));
+    [H,~] = svds(Attri(:,MaxEdges(1:min(10*d,size(Attri,2)))),d);
+else
+    [~,MaxEdges] = sort(sum(Net),'descend');
+    [H,~] = svds(Net(:,MaxEdges(1:min(10*d,n))),d);
+end
+if ~isempty(varargin)
+    lambda = varargin{1};
+    rho = varargin{2};
+    if length(varargin) >= 3
+        maxIter = varargin{3};
+        if length(varargin) >= 5
+            splitnum = varargin{5};
+        end
+    end
+end
+Block = min(ceil(n/splitnum),7575); % Treat each 7575 nodes as a block
+splitnum = ceil(n/Block);
+Z = diag(sum(Attri.^2,2).^-.5); % temporary value
+Z(isinf(Z)) = 0; % temporary value
+Attri = Attri'*Z; % Normalization
+%% First update H
+Z = H'; % Transpose for speedup
+XTX = Z*H*2; % Transpose for speedup
+H = Z; % Transpose for speedup
+for Blocki = 1:splitnum % Split nodes into different Blocks
+    IndexBlock = 1+Block*(Blocki-1); % First Index for splitting blocks
+    LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+    SA = Attri(:,LocalIndex)'*Attri; % Local affinity matrix for Blocki
+    sumS = Z*SA'*2;
+    for i = LocalIndex 
+        Neighbor = Z(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+        for j = 1:1
+            normi_j = sum((bsxfun(@minus,Neighbor,H(:,i))).^2).^.5; % norm of h_i^k-z_j^k
+            nzIdx = logical(normi_j); % Non-equal Index
+            if any(nzIdx)
+                Wij = lambda*Net(i,Net(:,i)~=0);
+                normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                H(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*Z(:,i));
+            else
+                H(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*Z(:,i));
+            end
+        end
+    end
+end
+Affi = Blocki; % Index for affinity matrix
+U = zeros(d,n);
+%% Iterations
+for iter = 1:maxIter-1
+    %% Update Z
+    XTX = H*H'*2;
+    for Blocki = 1:splitnum % Split nodes into different Blocks
+        IndexBlock = 1+Block*(Blocki-1); % Index for splitting blcks
+        LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+        if Affi ~= Blocki % check the cached SA is the needed or not
+            SA = Attri(:,LocalIndex)'*Attri;
+            Affi = Blocki; % Index for affinity matrix
+        end
+        sumS = H*SA'*2;
+        for i = LocalIndex
+            Neighbor = H(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+            for j = 1:1
+                normi_j = sum((bsxfun(@minus,Neighbor,Z(:,i))).^2).^.5;
+                nzIdx = logical(normi_j); % Non-equal Index
+                if any(nzIdx)
+                    Wij = lambda*Net(i,Net(:,i)~=0);
+                    normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                    Z(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*(H(:,i)+U(:,i)));
+                else
+                    Z(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*(H(:,i)+U(:,i)));
+                end
+            end
+        end
+    end
+    U = U+H-Z; % Update U
+    %% Update H
+    XTX = Z*Z'*2; % Transposed for speedup
+    for Blocki = 1:splitnum % Split nodes into different Blocks
+        IndexBlock = 1+Block*(Blocki-1); % Index for splitting blcks
+        LocalIndex = IndexBlock:IndexBlock-1+min(n-IndexBlock+1,Block);
+        if Affi ~= Blocki % check the cached SA is the needed or not
+            SA = Attri(:,LocalIndex)'*Attri;
+            Affi = Blocki; % Index for affinity matrix
+        end
+        sumS = Z*SA'*2;
+        for i = LocalIndex
+            Neighbor = Z(:,Net(:,i)~=0); % the set of adjacent nodes of node i
+            for j = 1:1
+                normi_j = sum((bsxfun(@minus,Neighbor,H(:,i))).^2).^.5;
+                nzIdx = logical(normi_j); % Non-equal Index
+                if any(nzIdx)
+                    Wij = lambda*Net(i,Net(:,i)~=0);
+                    normi_j = Wij(nzIdx)./normi_j(nzIdx);
+                    H(:,i)=(XTX+(sum(normi_j)+rho)*eye(d))\(sumS(:,i-IndexBlock+1)+sum(bsxfun(@times,Neighbor(:,nzIdx),normi_j),2)+rho*(Z(:,i)-U(:,i)));
+                else
+                    H(:,i)=(XTX+rho*eye(d))\(sumS(:,i-IndexBlock+1)+rho*(Z(:,i)-U(:,i)));
+                end
+            end
+        end
+    end
+end
+H = H'; % H is transposed
